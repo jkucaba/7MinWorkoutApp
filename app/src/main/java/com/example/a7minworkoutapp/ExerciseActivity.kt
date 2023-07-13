c91f1d067b8eb61f1302b9731a4eab0456fcf671
Jakub <jakub.kucaba@gmail.com>
2023-07-13 10:39:37 +0200

    adding countDownTimer

--- app/src/main/java/com/example/a7minworkoutapp/ExerciseActivity.kt
+++ app/src/main/java/com/example/a7minworkoutapp/ExerciseActivity.kt
@@ -2,11 +2,16 @@ package com.example.a7minworkoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import com.example.a7minworkoutapp.databinding.ActivityExerciseBinding
 import com.example.a7minworkoutapp.databinding.ActivityMainBinding
 
 class ExerciseActivity : AppCompatActivity() {
     private var binding: ActivityExerciseBinding? = null
+
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,5 +29,47 @@ class ExerciseActivity : AppCompatActivity() {
             onBackPressed()
         }
 
+        setupRestView()
+
     }
+
+    private fun setupRestView(){
+        if(restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+
+        setRestProgressBar()
+    }
+
+    private fun setRestProgressBar(){
+        binding?.progressBar?.progress = restProgress
+
+        restTimer = object : CountDownTimer(10000, 1000){
+            override fun onTick(p0: Long) {
+                restProgress++
+                binding?.progressBar?.progress = 10 - restProgress
+                binding?.tvTimer?.text = (10 - restProgress).toString()
+            }
+            override fun onFinish() {
+                Toast.makeText(
+                    this@ExerciseActivity,
+                    "Here now we will start the exercise",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }.start() //startujemy timer
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        if(restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+            binding = null
+
+    }
+
 }
